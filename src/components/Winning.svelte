7b3b5a05f9b4f85fda2ff6e6a20ef580b5973300
Tobias Karlsson <github@hoidi.se>
2024-05-29 14:07:46 +0200

    Add a replay button

--- src/components/Winning.svelte
+++ src/components/Winning.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { GameState } from '../types';
+	import Replay from './Replay.svelte';
 
 	export let gameState: GameState;
 
@@ -8,8 +9,13 @@
 
 <div class="flex justify-center items-center text-slate-200 w-full h-full">
 	{#if gameState.isGameOver}
-		<div class="text-6xl">
-			Player {gameState.currentPlayer} won
+		<div class="flex flex-col justify-center">
+			<div class="w-full text-7xl font-bold font-sans">
+				Player {gameState.currentPlayer} won
+			</div>
+			<div class="w-full h-[10%] bg-orange-400">
+				<Replay bind:gameState />
+			</div>
 		</div>
 	{/if}
 </div>
